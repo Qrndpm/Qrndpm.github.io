838a67f3a3ce9fc57dc3350e17d850c2c70401ca
Qrndpm <31998789+Qrndpm@users.noreply.github.com>
2022-11-13 15:42:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function testJSON(){
    var request = new XMLHttpRequest();
-   request.open("GET", "list.xml", false);
+   request.open("GET", "list.json", false);
    request.send(null)
    var my_JSON_object = JSON.parse(request.responseText);
    alert (my_JSON_object.result[0]);
