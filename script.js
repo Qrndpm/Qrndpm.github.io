2ab9d8a0d6716de929f38c1753e189b3e4a7d95e
Qrndpm <31998789+Qrndpm@users.noreply.github.com>
2022-11-13 20:20:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,16 @@ function testJSON(){
    var bonnereponse = Math.floor(Math.random() * 4);
    var bonBouton = document.getElementById('reponse'+bonnereponse);
    bonBouton.innerText = my_JSON_object['caracteres'][indexCar]['pinyin'];
+   bonBouton.value = "Vrai";
+   for (let i = 0; i<4; i++){
+      let bouton;
+      if(i!=bonnereponse){
+         bouton = document.getElementById('reponse'+i);
+         bouton.value = "Faux";
+         do{
+            n = Math.floor(Math.random() * my_JSON_object['caracteres'].length);
+         }while(n==indexCar);
+         bouton.innerText my_JSON_object['caracteres'][n]['pinyin'];
+      }
+   }
 }
