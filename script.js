0414e9665e01b72d8027003fd841291a77218ace
Qrndpm <31998789+Qrndpm@users.noreply.github.com>
2022-11-13 16:11:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ function testJSON(){
    request.send(null)
    var my_JSON_object = JSON.parse(request.responseText);
    console.log(my_JSON_object);
+   console.log(my_JSON_object[0]);
+   console.log(my_JSON_object[0].car);
+   console.log(my_JSON_object[0].pinyin);
+   console.log(my_JSON_object[1]);
+   console.log(my_JSON_object[2]);
+   console.log(my_JSON_object.length)
+   var element = document.getElementById('car');
+   element.TEXT_NODE = "TEST";
 }
